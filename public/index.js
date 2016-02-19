f62038aa6e5a388976634550a128c731685d0da5
frezmecritus <frezmecritus@gmail.com>
2016-02-18 22:10:30 -0800

    Include react-lightbox, start merging

--- public/index.js
+++ public/index.js
@@ -41,20 +41,10 @@ var Canvas = React.createClass({
 	renderContent: function() {
 		var res;
 		var page = this.state.currPage;
-		if (page=="WORK")
+		if (page=="WORK") 
 			res = <div><div>
-				<a><img src="image/img1.png"></img><div>Street Accidents Prediction</div></a>
-				<a><img src="image/img4.png"></img><div>WebGL Physics Simulation System</div></a>
-				<a><img src="image/img3.png"></img><div>Geospatial Data Visualization</div></a>
-			</div><div>
-				<a><img src="image/img7.png"></img><div>Big Lake Buoy</div></a>
-				<a><img src="image/img5.png"></img><div>Medicare Service Platform</div></a>
-				<a><img src="image/img6.png"></img><div>Power Hour</div></a>
-			</div><div>
-				<a><img src="image/img2.png"></img><div>Flying Airplane Navigation</div></a>
-				<a><img src="image/img8.png"></img><div>Le Petite Chef</div></a>
-				<a><img src="image/img9.png"></img><div>OpenGL Transparency</div></a>
-			</div></div>;
+				<Lightbox pictures={['image/img1.png','image/img2.png','image/img3.png','image/img4.png']} keyboard controls={Controls}/>
+				</div></div>
 		else if (page=="ABOUT")
 			res = <About />;
 		else
@@ -67,6 +57,19 @@ var Canvas = React.createClass({
 		return <div id="footer"> Designed and developed by {author} @2016 </div>;
 	}
 });
+			// res = <div><div>
+			// 	<a><img src="image/img1.png"></img><div>Street Accidents Prediction</div></a>
+			// 	<a><img src="image/img4.png"></img><div>WebGL Physics Simulation System</div></a>
+			// 	<a><img src="image/img3.png"></img><div>Geospatial Data Visualization</div></a>
+			// </div><div>
+			// 	<a><img src="image/img7.png"></img><div>Big Lake Buoy</div></a>
+			// 	<a><img src="image/img5.png"></img><div>Medicare Service Platform</div></a>
+			// 	<a><img src="image/img6.png"></img><div>Power Hour</div></a>
+			// </div><div>
+			// 	<a><img src="image/img2.png"></img><div>Flying Airplane Navigation</div></a>
+			// 	<a><img src="image/img8.png"></img><div>Le Petite Chef</div></a>
+			// 	<a><img src="image/img9.png"></img><div>OpenGL Transparency</div></a>
+			// </div></div>;
 
 var About = React.createClass({
 	render: function() {
@@ -98,40 +101,16 @@ var Contact = React.createClass({
 	}
 });
 
-var TodoList = React.createClass({
-	getInitialState: function() {
-		return {items: ['hello', 'world', 'click', 'me'], showed: false};
-	},
-
-	handleClick: function(i) {
-		if (this.state.showed) {
-			var newItems = this.state.items.slice();
-			newItems.splice(newItems.length-1, 1);
-			this.setState({items: newItems, showed: false});
-		}
-		else {
-			var newItems = this.state.items.concat(['dsfsfd']);
-			this.setState({items: newItems, showed: true});
-		}
-	},
-	render: function() {
-		var items = (<div key='Hello' onClick={this.handleClick}>Hello</div>);
-		var items2 = this.state.items.map(function(item, i) {
-			return (
-				<div key={item} onClick={this.handleClick.bind(this, i)}>
-					{item}
-				</div>
-			);
-		}.bind(this));
-		return (
-			<div>
-				<ReactCSSTransitionGroup transitionName="example" transitionAppear={true} transitionAppearTimeout={500}>
-					<img src="image/img1.png"></img>
-				</ReactCSSTransitionGroup>
-			</div>
-		);
-	}
-});
-
 const appRoot = document.getElementById('canvas');
 ReactDOM.render(<Canvas />, appRoot);
+// React.render(
+//   <Lightbox
+//     pictures={[
+//       'image/img1.png',
+//       'image/img2.png',
+//       'image/img3.png',
+//       'image/img4.png'
+//     ]}
+//     keyboard
+//     controls={Controls}/>
+// , appRoot);
